f17d071948a874cf984b88717b9ef155d650df80
Beautyya <89833664+Beautyya@users.noreply.github.com>
2022-01-12 20:01:21 +0800

    Add files via upload

---
+++ torch_demo/model.py
@@ -0,0 +1,23 @@
+from torch import nn
+from torch.nn import Sequential, Conv2d, MaxPool2d, Flatten, Linear
+
+
+class ExModel(nn.Module):
+    def __init__(self):
+        super(ExModel, self).__init__()
+        self.seq = Sequential(
+            Conv2d(3, 32, 5, padding=2),
+            MaxPool2d(2),
+            Conv2d(32, 32, 5, padding=2),
+            MaxPool2d(2),
+            Conv2d(32, 64, 5, padding=2),
+            MaxPool2d(2),
+            Flatten(),
+            Linear(1024, 64),
+            Linear(64, 10)
+        )
+
+    def forward(self, x):
+        x = self.seq(x)
+        return x
+
